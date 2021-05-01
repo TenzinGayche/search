5d90e52a1fd4b1aac5ca702ba7e96c6efb85dbee
TenzinGayche <tenzingayche@TenzinGayches-MacBook-Pro.local>
2021-05-01 21:56:54 +0530

    first commit

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://user:Gayche6410@cluster0.pntza.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const items = require("./routes/api/items");
+const app = express();
+//bodyparser middleware
+app.use(bodyParser.json());
+//DB config
+const db = require("./config/key").mongoURI;
+//connect to MOngo
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+
+  .then(() => console.log("MongoD connected"))
+  .catch((err) => console.log(err));
+app.use('/api/items', items);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
